735d24d24d22fe1ec72873c6cfd0fa9c6c5359bd
michaelperez <michael.perez@yahoo.com>
2021-09-11 13:53:16 -0500

    Add logging to the application.

--- src/main/java/com/example/demo/controllers/OrderController.java
+++ src/main/java/com/example/demo/controllers/OrderController.java
@@ -4,6 +4,8 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.UserOrder;
 import com.example.demo.model.persistence.repositories.OrderRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.List;
 @RequestMapping("/api/order")
 public class OrderController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(OrderController.class);
 
 	private UserRepository userRepository;
 
@@ -26,21 +29,32 @@ public class OrderController {
 	
 	@PostMapping("/submit/{username}")
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
+		LOG.info("Submit order for user {}.",username);
+
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			LOG.error("User {} not found.", username);
 			return ResponseEntity.notFound().build();
 		}
+
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
+		LOG.info("User order {} has been successfully saved.", order.getId());
 		return ResponseEntity.ok(order);
 	}
 	
 	@GetMapping("/history/{username}")
 	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
+		LOG.info("Get orders for user {}.",username);
+
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			LOG.error("User {} not found.", username);
 			return ResponseEntity.notFound().build();
 		}
-		return ResponseEntity.ok(orderRepository.findByUser(user));
+
+		List<UserOrder> orders = orderRepository.findByUser(user);
+		LOG.info("{} orders were found.", orders.size());
+		return ResponseEntity.ok(orders);
 	}
 }
--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 
 	private UserRepository userRepository;
 
@@ -27,22 +31,35 @@ public class UserController {
 
 	@GetMapping("/id/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
+		LOG.info("Find user by id {}.",id);
 		return ResponseEntity.of(userRepository.findById(id));
 	}
 	
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
+		LOG.info("Find user by username {}.",username);
 		User user = userRepository.findByUsername(username);
+
+		if (user == null) {
+			LOG.error("User {} not found.",username);
+		} else {
+			LOG.info("User {} found.", username);
+		}
+
 		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
 	}
 	
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		LOG.info("Create user {}",createUserRequest.getUsername());
+
 		if (userRepository.findByUsername(createUserRequest.getUsername()) != null) {
+			LOG.error("User {} already exists.", createUserRequest.getUsername());
 			return ResponseEntity.badRequest().build();
 		}
 		if (createUserRequest.getPassword().length() < 7 ||
 				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+			LOG.error("Password for user {} does not meet requirements.", createUserRequest.getUsername());
 			return ResponseEntity.badRequest().build();
 		}
 
@@ -55,6 +72,7 @@ public class UserController {
 		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 		userRepository.save(user);
 
+		LOG.info("User {} successfully created.", createUserRequest.getUsername());
 		return ResponseEntity.ok(user);
 	}
 	
